9de420fb463dd602bec2cbf0a536547b16d6e450
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2017-09-07 13:29:52 +0000

    initial Commit

---
+++ js/main.js
@@ -0,0 +1,113 @@
+
+function main() {
+
+(function () {
+   'use strict';
+
+   // Testimonial slider
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+  	$(document).ready(function() {
+  	    $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
+
+	// affix the navbar after scroll below header
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+
+	// skills chart
+	$(document).ready(function(e) {
+	//var windowBottom = $(window).height();
+	var index=0;
+	$(document).scroll(function(){
+		var top = $('#skills').height()-$(window).scrollTop();
+    
+		if(top<-1000){
+			if(index==0){
+
+				$('.chart').easyPieChart({
+					easing: 'easeOutBounce',
+					onStep: function(from, to, percent) {
+						$(this.el).find('.percent').text(Math.round(percent));
+					}
+				});
+
+				}
+			index++;
+		}
+	})
+	//console.log(nagativeValue)
+	});
+
+
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+	// counterUp
+
+
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1000 // the speed time in ms
+			});
+		}
+	});
+
+  	// Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});
+
+}());
+
+
+}
+main();
